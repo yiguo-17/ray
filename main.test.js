d889109308ebefda626bda4395f245c798179f4e
Colin Jaffe <balloonasaurus@gmail.com>
2020-07-29 09:26:07 -0400

    Copies tests over from solution repo.

--- main.test.js
+++ main.test.js
@@ -1,29 +1,27 @@
-const Ray = require('./main.js');
-
-let ray;
-
-beforeEach(() => {
-  ray = Ray();
-})
+const Ray = require('./main.js')
 
 describe('Ray', () => {
   it('returns an object', () => {
-    expect(typeof Ray()).toBe('object');
+    const ray = Ray();
+    expect(typeof ray).toBe('object');
   })
 })
 
 describe('Ray.length', () => {
   it(`begins at 0`, () => {
+    const ray = Ray();
     expect(ray.length).toBe(0);
   })
 })
 
 describe('Ray.push', () => {
   it(`is a method`, () => {
+    const ray = Ray();
     expect(typeof ray.push).toBe('function')
   })
 
   it(`puts the given value as a value in the returned object`, () => {
+    const ray = Ray();
     ray.push(3);
     expect(Object.values(ray).includes(3)).toBe(true);
     ray.push(5);
@@ -31,6 +29,7 @@ describe('Ray.push', () => {
   })
 
   it(`increments the length by 1`, () => {
+    const ray = Ray();
     ray.push(7);
     expect(ray.length).toBe(1);
     ray.push(11);
@@ -40,6 +39,7 @@ describe('Ray.push', () => {
   })
 
   it(`puts the given value at the first available number key`, () => {
+    const ray = Ray();
     ray.push(7);
     expect(ray[0]).toBe(7);
     ray.push(11);
@@ -49,10 +49,12 @@ describe('Ray.push', () => {
 
 describe('Ray.pop', () => {
   it(`is a method`, () => {
+    const ray = Ray();
     expect(typeof ray.pop).toBe('function')
   })
 
   it(`decrements the length by 1`, () => {
+    const ray = Ray();
     ray[0] = 5;
     ray[1] = 5;
     ray.length = 2;
@@ -64,6 +66,7 @@ describe('Ray.pop', () => {
 
 
   it(`removes the value at the highest used number key`, () => {
+    const ray = Ray();
     ray[0] = 5;
     ray[1] = 7;
     ray.length = 2;
@@ -75,6 +78,7 @@ describe('Ray.pop', () => {
   })
 
   it(`returns the removed value`, () => {
+    const ray = Ray();
     ray[0] = 15;
     ray[1] = 51;
     ray.length = 2;
@@ -85,50 +89,94 @@ describe('Ray.pop', () => {
   })
 })
 
-describe('Ray.unshift', () => {
-  it(`is a method`, () => {
-    expect(typeof ray.unshift).toBe('function')
+describe('Ray.includes', () => {
+  it('returns true if the given value is a element in the object', () => {
+    const ray = Ray();
+    ray[0] = 'hello';
+    ray[1] = 'oh hi';
+    ray[2] = 'goodnight';
+    ray.length = 3;
+
+    expect(ray.includes('hello')).toBe(true);
+    expect(ray.includes('oh hi')).toBe(true);
+    expect(ray.includes('goodnight')).toBe(true);
   })
 
-  it(`puts the given value as a value in the returned object`, () => {
-    ray.unshift(3);
-    expect(Object.values(ray).includes(3)).toBe(true);
-    ray.unshift(5);
-    expect(Object.values(ray).includes(5)).toBe(true);
+  it('returns false if the given value is not an element in the object', () => {
+    const ray = Ray();
+    ray[0] = 'hello';
+    ray[1] = 'oh hi';
+    ray[2] = 'goodnight';
+    ray.length = 3;
+
+    expect(ray.includes('nope')).toBe(false);
+    expect(ray.includes('not in here')).toBe(false);
+    expect(ray.includes('no one here but us chickens')).toBe(false);
   })
 
-  it(`increments the length by 1`, () => {
-    ray.unshift(7);
-    expect(ray.length).toBe(1);
-    ray.unshift(11);
-    expect(ray.length).toBe(2);
-    ray.unshift(11);
-    expect(ray.length).toBe(3);
+  it('returns false even if the given value is within an element in the object', () => {
+    const ray = Ray();
+    ray[0] = 'hello';
+    ray[1] = 'hi';
+    ray[2] = 'goodnight';
+    ray.length = 3;
+
+    expect(ray.includes('hello there')).toBe(false);
+    expect(ray.includes('oh hi')).toBe(false);
+    expect(ray.includes('goodnight sweet prince')).toBe(false);
+  })
+})
+
+describe('Ray.indexOf', () => {
+  it('returns the index (key) at which the given value occurs in the object', () => {
+    const ray1 = Ray();
+    const ray2 = Ray();
+    ray1[0] = 'Michaelangelo';
+    ray1[1] = 'Leonardo';
+    ray1[2] = 'Rafael';
+    ray1[3] = 'Donatello';
+    ray1.length = 4;
+    ray2[0] = 'Gaugin';
+    ray2[1] = 'Rodin';
+    ray2[2] = 'Dali';
+    ray2[3] = 'Degas';
+    ray2[4] = 'Goya';
+    ray2.length = 5;
+    expect(ray1.indexOf('Rafael')).toBe(2);
+    expect(ray1.indexOf('Michaelangelo')).toBe(0);
+    expect(ray2.indexOf('Rodin')).toBe(1);
+    expect(ray2.indexOf('Goya')).toBe(4);
   })
 
-  it(`puts the given value at index 0`, () => {
-    ray.unshift(7);
-    expect(ray[0]).toBe(7);
-    ray.unshift(11);
-    expect(ray[0]).toBe(11);
-  })
-
-  it(`pushes all other values to the next index`, () => {
-    ray.unshift(7);
-    ray.unshift(11);
-    ray.unshift(12);
-    expect(ray[0]).toBe(12);
-    expect(ray[1]).toBe(11);
-    expect(ray[2]).toBe(7);
+  it('returns -1 if the value is not at any key in the object', () => {
+    const ray1 = Ray();
+    const ray2 = Ray();
+    ray1[0] = 'Michaelangelo';
+    ray1[1] = 'Leonardo';
+    ray1[2] = 'Rafael';
+    ray1[3] = 'Donatello';
+    ray1.length = 4;
+    ray2[0] = 'Gaugin';
+    ray2[1] = 'Rodin';
+    ray2[2] = 'Dali';
+    ray2[3] = 'Degas';
+    ray2[4] = 'Goya';
+    ray2.length = 5;
+    expect(ray1.indexOf('Splinter')).toBe(-1);
+    expect(ray1.indexOf('Warhol')).toBe(-1);
+    expect(ray2.indexOf('Shredder')).toBe(-1);
+    expect(ray2.indexOf('Banksy')).toBe(-1);
   })
 })
 
 describe('Ray.shift', () => {
   it(`is a method`, () => {
+    const ray = Ray();
     expect(typeof ray.shift).toBe('function')
   })
 
   it(`decrements the length by 1`, () => {
+    const ray = Ray();
     ray[0] = 5;
     ray[1] = 'hello';
     ray[2] = false;
@@ -142,6 +190,7 @@ describe('Ray.shift', () => {
   })
 
   it(`removes the value and key at the previous last index`, () => {
+    const ray = Ray();
     ray[0] = 7;
     ray[1] = 11;
     ray.length = 2;
@@ -150,6 +199,7 @@ describe('Ray.shift', () => {
   })
 
   it(`pushes all elements but the first to a key one lower`, () => {
+    const ray = Ray();
     ray[0] = 7;
     ray[1] = 11;
     ray[2] = 12;
@@ -162,6 +212,7 @@ describe('Ray.shift', () => {
   })
 
   it(`returns the removed element`, () => {
+    const ray = Ray();
     ray[0] = 15;
     ray[1] = 51;
     ray.length = 2;
@@ -169,5 +220,48 @@ describe('Ray.shift', () => {
     expect(removed1).toBe(15)
     const removed2 = ray.shift();
     expect(removed2).toBe(51);
+  })
+})
+
+describe('Ray.unshift', () => {
+  it(`is a method`, () => {
+    const ray = Ray();
+    expect(typeof ray.unshift).toBe('function')
+  })
+
+  it(`puts the given value as a value in the returned object`, () => {
+    const ray = Ray();
+    ray.unshift(3);
+    expect(Object.values(ray).includes(3)).toBe(true);
+    ray.unshift(5);
+    expect(Object.values(ray).includes(5)).toBe(true);
+  })
+
+  it(`increments the length by 1`, () => {
+    const ray = Ray();
+    ray.unshift(7);
+    expect(ray.length).toBe(1);
+    ray.unshift(11);
+    expect(ray.length).toBe(2);
+    ray.unshift(11);
+    expect(ray.length).toBe(3);
+  })
+
+  it(`puts the given value at index 0`, () => {
+    const ray = Ray();
+    ray.unshift(7);
+    expect(ray[0]).toBe(7);
+    ray.unshift(11);
+    expect(ray[0]).toBe(11);
+  })
+
+  it(`pushes all other values to the next index`, () => {
+    const ray = Ray();
+    ray.unshift(7);
+    ray.unshift(11);
+    ray.unshift(12);
+    expect(ray[0]).toBe(12);
+    expect(ray[1]).toBe(11);
+    expect(ray[2]).toBe(7);
   })
 })
